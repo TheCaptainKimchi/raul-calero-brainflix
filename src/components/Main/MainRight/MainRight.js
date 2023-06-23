8759d4cba4a6f2c22fe6db95e98b896f8231ae13
Raul Calero <raulcalero7@gmail.com>
2023-06-22 21:46:49 -0700

    Added base components for MainRight component adding in 1 card template with an image, title, and author.

--- src/components/Main/MainRight/MainRight.js
+++ src/components/Main/MainRight/MainRight.js
@@ -1,5 +1,25 @@
+import "../../../styles/MainRight.scss";
+
 function MainRight() {
-  return <div></div>;
+  return (
+    <div className="videos">
+      <h3 className="videos__title">NEXT VIDEOS</h3>
+      <div className="videos__container">
+        <div className="videos__container-card">
+          <div>
+            <img
+              className="videos__container-card-image"
+              src="https://i.imgur.com/5qyCZrD.jpg"
+            ></img>
+          </div>
+          <div className="videos__container-card-text">
+            <h3>Become A Travel Pro In One Easy Lesson</h3>
+            <p>Todd Welch</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainRight;
