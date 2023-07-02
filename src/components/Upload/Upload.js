94b0a1d3a59a990a1bedf59575c2c394bb71e2ca
Raul Calero <raulcalero7@gmail.com>
2023-07-01 23:42:33 -0700

    Completed upload component page adding in title, image, upload form with buttons to publish or cancel. Also added responsive design for tablet and desktop.

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,33 +1,45 @@
 import "./Upload.scss";
-import Header from "../Header/Header";
 import UploadPreview from "../../assets/images/Upload-video-preview.jpg";
-import Icon from "../../assets/images/icons/upload.svg";
+import icon from "../../assets/images/icons/publish.svg";
 
 const Upload = () => {
+  function clickHandler(e) {
+    e.preventDefault();
+    window.location.href = "/";
+  }
   return (
     <div className="upload">
-      <Header />
       <h1 className="upload__title">Upload Video</h1>
-      <div className="upload__left">
-        <p className="upload__left-title">VIDEO THUMBNAIL</p>
-        <img src={UploadPreview}></img>
-      </div>
+      <div className="upload__left-right">
+        <div className="upload__left">
+          <p className="upload__left-title">VIDEO THUMBNAIL</p>
+          <img src={UploadPreview}></img>
+        </div>
+        <div className="upload__right">
+          <form className="upload__right-form">
+            <label className="upload__right-form-title upload__right-form-title--1">
+              TITLE YOUR VIDEO
+            </label>
+            <input className="upload__right-form-title-input input"></input>
 
-      <div className="upload__right">
-        <form className="upload__right-form">
-          <label className="upload__right-form-title upload__right-form-title--1">
-            TITLE YOUR VIDEO
-          </label>
-          <input className="upload__right-form-title-input"></input>
-          <label className="upload__right-form-title upload__right-form-title--2">
-            ADD A VIDEO DESCRIPTION
-          </label>
-          <input className="upload__right-form-description-input"></input>
-          <a>
-            <img src={Icon}></img>
-            <button>PUBLISH</button>
-          </a>
-        </form>
+            <label className="upload__right-form-title upload__right-form-title--2">
+              ADD A VIDEO DESCRIPTION
+            </label>
+            <input className="upload__right-form-description-input input"></input>
+            <a className="upload__right-form-container">
+              <button
+                className="upload__right-form-button"
+                onClick={clickHandler}
+              >
+                <img src={icon}></img>
+                <p>PUBLISH</p>
+              </button>
+            </a>
+            <a className="upload__right-cancel" href="/">
+              <p className="upload__right-form-cancel">CANCEL</p>
+            </a>
+          </form>
+        </div>
       </div>
     </div>
   );
