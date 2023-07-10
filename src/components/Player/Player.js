dd5de5b21b5ef9f55b656629f80ac2f45154a1f8
Raul Calero <raulcalero7@gmail.com>
2023-07-10 07:54:08 -0700

    Player adjusted to take dynamic URL from localhost API.

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,9 +1,19 @@
 import "./Player.scss";
+import { useState, useEffect } from "react";
 
 function Player({ ApiDetails }) {
+  const [imageURL, setImageURL] = useState("");
+
+  useEffect(() => {
+    setImageURL(ApiDetails.image);
+  }, [ApiDetails.image]);
+
   return (
     <div className="player">
-      <video poster={ApiDetails.image} controls></video>
+      <video
+        poster={`http://localhost:8080/images/${imageURL}`}
+        controls
+      ></video>
     </div>
   );
 }
